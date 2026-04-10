21b53742646fd0c6f429b58857bb67511a467205
Kare-Lk <fa.cortesplacencia@gmail.com>
2026-04-10 12:06:30 -0400

    feat(tasks): base-flow-on-task-&-guard
    * jwt-strategy and guard created for validation on task controller.
    * controller and service basic flow implemented.

--- src/modules/tasks/tasks.controller.ts
+++ src/modules/tasks/tasks.controller.ts
@@ -1,4 +1,83 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  UseGuards,
+  Post,
+  Get,
+  Patch,
+  Delete,
+  Body,
+  Param,
+  Req,
+} from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { TasksService } from './tasks.service';
+import { Task } from './entities/task.entity';
 
+@UseGuards(JwtAuthGuard)
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Post()
+  create(
+    @Body()
+    body: {
+      title: string;
+      description?: string;
+    },
+    @Req()
+    req: {
+      user: {
+        userId: string;
+      };
+    },
+  ) {
+    return this.tasksService.create(
+      req.user.userId,
+      body.title,
+      body?.description,
+    );
+  }
+
+  @Get()
+  findAll(
+    @Req()
+    req: {
+      user: {
+        userId: string;
+      };
+    },
+  ) {
+    return this.tasksService.findAll(req.user.userId);
+  }
+
+  @Patch(':id/status')
+  updateStatus(
+    @Param('id') id: string,
+    @Body()
+    body: {
+      status: Task['status'];
+    },
+    @Req()
+    req: {
+      user: {
+        userId: string;
+      };
+    },
+  ) {
+    return this.tasksService.updateStatus(req.user.userId, id, body.status);
+  }
+
+  @Delete(':id')
+  remove(
+    @Param('id') id: string,
+    @Req()
+    req: {
+      user: {
+        userId: string;
+      };
+    },
+  ) {
+    return this.tasksService.remove(req.user.userId, id);
+  }
+}
--- src/modules/tasks/tasks.service.ts
+++ src/modules/tasks/tasks.service.ts
@@ -1,4 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Task } from './entities/task.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(
+    @InjectRepository(Task) private readonly taskRepository: Repository<Task>,
+  ) {}
+
+  async create(userId: string, title: string, description?: string) {
+    const newTask = this.taskRepository.create({
+      title,
+      description,
+      user: { id: userId },
+    });
+
+    return this.taskRepository.save(newTask);
+  }
+
+  async findAll(userId: string) {
+    return this.taskRepository.find({
+      where: {
+        user: { id: userId },
+        is_deleted: false,
+      },
+    });
+  }
+
+  async updateStatus(userId: string, taskId: string, status: Task['status']) {
+    const task = await this.taskRepository.findOne({
+      where: {
+        id: taskId,
+        user: { id: userId },
+      },
+    });
+
+    if (!task) throw new NotFoundException('Task not found');
+
+    task.status = status;
+    return this.taskRepository.save(task);
+  }
+
+  async remove(userId: string, taskId: string) {
+    const task = await this.taskRepository.findOne({
+      where: {
+        id: taskId,
+        user: { id: userId },
+      },
+    });
+
+    if (!task) throw new NotFoundException('Task not found');
+
+    task.is_deleted = true;
+    return this.taskRepository.save(task);
+  }
+}
